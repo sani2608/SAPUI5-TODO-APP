1888d8d56d2f9d28ed0896fe20121c70e631bdde
sanikumar <sanikumarsahani@gmail.com>
2022-10-17 13:53:18 +0530

    Updates

---
+++ Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","com/sani/todo/model/models"],function(e,t,i){"use strict";return e.extend("com.sani.todo.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
